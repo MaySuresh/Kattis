0fec0ba516c700be4036a940952d261f161fe2de
MaySuresh <90007893+MaySuresh@users.noreply.github.com>
2022-01-20 12:21:24 -0500

    first commit

---
+++ outlets.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class main {
+
+    public static void main(String [] args){
+        Scanner sc = new Scanner (System.in);
+        int TC = sc.nextInt();
+        
+
+        for(int i=0;i<TC;i++){int k = sc.nextInt();
+            int fin =0;
+            for (int j =0;j<k;j++){ int O = sc.nextInt();
+                fin += O;
+                
+
+            }
+        System.out.println(fin-k+1);
+        }
+        
+
+    }
+    
+}
