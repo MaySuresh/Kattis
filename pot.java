41562f747bdc93799b48affd9b29ffee5c8addda
MaySuresh <90007893+MaySuresh@users.noreply.github.com>
2022-01-20 12:46:56 -0500

    Upload

---
+++ pot.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class pot {
+    public static void main(String [] args){
+        Scanner  sc = new Scanner(System.in);
+       int fin=0;
+       
+        int N = sc.nextInt();
+        
+        
+        for(int i=0;i<N;i++){
+            int inp = sc.nextInt();
+            int last = inp%10;
+            int first = inp/10;
+            fin+= Math.pow(first,last);
+        }
+        System.out.println(fin);
+
+
+
+    }
+}
