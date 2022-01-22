f61f239f4a4652f138541beedeb0ed5cca4a450b
MaySuresh <90007893+MaySuresh@users.noreply.github.com>
2022-01-22 00:09:22 -0500

    Update

---
+++ Java/oddecho.java
@@ -0,0 +1,29 @@
+package Java;
+import java.util.*;
+
+class oddecho {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String e;
+        
+        for(int i =0;i<N+1;i++){
+            e = sc.nextLine();
+
+            if(i%2!=0){
+                System.out.println(e);
+
+            }
+            
+            
+
+        }
+
+        
+           
+        
+
+
+    }
+    
+}
